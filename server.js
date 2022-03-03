19284c12906f4af79beae549b32b70c83268d276
jonyprill <jonathantprill@gmail.com>
2022-03-03 11:52:39 -0600

    added initial links to pages and server

---
+++ server.js
@@ -0,0 +1,37 @@
+// The following HTML routes should be created:
+//  -GET /notes should return the notes.html file.
+//  -GET * should return the index.html file.
+
+// The following API routes should be created:
+//  -GET /api/notes should read the db.json file and return all saved notes as JSON.
+//  -POST /api/notes should receive a new note to save on the request body, add it to
+//      the db.json file, and then return the new note to the client. You'll need to find
+//      a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
